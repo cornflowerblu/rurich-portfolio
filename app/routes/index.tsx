efa0d8c7b51a0b595436286c1ea6c2901d795dab
Roger <53064994+cornflowerblu@users.noreply.github.com>
2022-04-13 23:26:48 -0700

    Update Homepage Banner (#1)
    * Update header text
    
    * This was a mistake - updated banner text instead

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,8 +24,7 @@ export default function Index() {
                 </span>
               </h1>
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-                Check the README.md file for instructions on how to get this
-                project deployed.
+                This EPIC portfolio showcase is gonna be for RURICH on the JAMstack!
               </p>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
