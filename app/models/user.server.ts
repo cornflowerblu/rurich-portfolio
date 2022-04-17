6cb3e0f6918e4a9ea1d76f08c69a480733eab998
Roger Urich <rogerdurich+git@gmail.com>
2022-04-17 00:30:48 -0700

    added the concept of an author to the create user model

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,9 +1,27 @@
 import type { Password, User } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import { GraphQLClient, gql } from "graphql-request";
 
 import { prisma } from "~/db.server";
 
 export type { User } from "@prisma/client";
+
+export type Author = {
+  name: string;
+  intro: string;
+  slug: string;
+};
+
+export function slugify(name: String) {
+  return name
+    .toString()
+    .normalize("NFKD")
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w\-]+/g, "")
+    .replace(/\-\-+/g, "-");
+}
 
 export async function getUserById(id: User["id"]) {
   return prisma.user.findUnique({ where: { id } });
@@ -13,8 +31,37 @@ export async function getUserByEmail(email: User["email"]) {
   return prisma.user.findUnique({ where: { email } });
 }
 
-export async function createUser(email: User["email"], password: string) {
+export async function createUser(
+  email: User["email"],
+  password: string,
+  name: string,
+  intro: string
+) {
   const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Associate the user with a newly created Author in GraphCMS. 
+  const endpoint = "https://api.graph.cool/simple/v1/cixos23120m0n0173veiiwrjr";
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: "Bearer " + process.env.WRITE_TOKEN,
+    },
+  });
+
+  const mutation = gql`
+  mutation {
+    createAuthor($name: String!, $intro: String!, $bio: String!, $slug: String!) {
+      id
+    }
+  }`;
+
+  const authorInput: Author = {
+    name: name,
+    intro: intro,
+    slug: slugify(name),
+  };
+
+  const authorId = await graphQLClient.request(mutation, authorInput);
 
   return prisma.user.create({
     data: {
@@ -24,6 +71,7 @@ export async function createUser(email: User["email"], password: string) {
           hash: hashedPassword,
         },
       },
+      authorId: authorId,
     },
   });
 }
